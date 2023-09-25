e5009fff6b6a08019ebe7b354dd46a5f03dea59b
AdityaChowdhury2 <adityac5102@gmail.com>
2023-09-26 01:06:02 +0600

    see all button conditional rendaring

--- src/Page/DonationPage/DonationPage.jsx
+++ src/Page/DonationPage/DonationPage.jsx
@@ -7,35 +7,46 @@ import DonatedCampaignCard from '../../components/DonatedCampaignCard/DonatedCam
 const DonationPage = () => {
 	const { donatedCampaigns } = useContext(DonationCampaignContext);
 	const [isShowAll, setShowAll] = useState(false);
+
 	return (
 		<div className="container mt-12">
 			<div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-				{isShowAll
-					? donatedCampaigns.map(campaign => (
-							<DonatedCampaignCard key={campaign.id} campaign={campaign} />
-					  ))
-					: donatedCampaigns
+				{!isShowAll
+					? donatedCampaigns
 							.slice(0, 4)
 							.map(campaign => (
 								<DonatedCampaignCard key={campaign.id} campaign={campaign} />
-							))}
+							))
+					: donatedCampaigns.map(campaign => (
+							<DonatedCampaignCard key={campaign.id} campaign={campaign} />
+					  ))}
 			</div>
 			<div className="flex justify-center mt-10">
-				{!isShowAll ? (
+				{!isShowAll && donatedCampaigns.length > 4 && (
+					<button
+						className="bg-green-500 px-3 py-2 rounded text-white"
+						onClick={() => setShowAll(true)}
+					>
+						Show All
+					</button>
+				)}
+				{/* {!isShowAll && donatedCampaigns.length > 4 ? (
 					<button
 						className="bg-green-500 px-3 py-2 rounded text-white"
 						onClick={() => setShowAll(!isShowAll)}
 					>
 						Show All
 					</button>
-				) : (
+				) : donatedCampaigns.length || isShowAll ? (
 					<button
 						className=" bg-red-500 px-3 py-2 rounded text-white"
 						onClick={() => setShowAll(!isShowAll)}
 					>
 						View Less
 					</button>
-				)}
+				) : (
+					<div className="flex justify-center h-[30vh]">No Data Found</div>
+				)} */}
 			</div>
 		</div>
 	);
